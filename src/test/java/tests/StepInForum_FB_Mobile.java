09c3d2682f3c69b4f6f727257ba1c350c736bf17
DDS-NA\pjain <pjain@mediaocean.com>
2019-09-26 12:36:14 +0530

    mobile

--- src/test/java/tests/StepInForum_FB_Mobile.java
+++ src/test/java/tests/StepInForum_FB_Mobile.java
@@ -1,29 +1,32 @@
 package tests;
-
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import core.BaseTest;
 import pageobjects.mobile.MobilePageObjects;
 import utilities.FileUtils;
-
 public class StepInForum_FB_Mobile extends BaseTest{
-	
-	@Test
-    public void method() throws Exception{
-//		appiumDriver.get("https://www.google.com");
-		MobilePageObjects mpo = new MobilePageObjects(appiumDriver);
-		mpo.searchOnGoogle();
-		mpo.clickFbPost();
-		mpo.openFbPost();
-		mpo.openPhotosPost();
-		mpo.clickImage();
-		int imageCounter=1;
-		FileUtils fileUtils = new FileUtils();
-		while(imageCounter<=5) {
-			String filePath = System.getProperty("user.dir") + "/src/test/resources/mobilephotos/" + imageCounter + ".jpg";
-			int size = fileUtils.getFileSizeInKb(filePath);
-		}
+    
+    MobilePageObjects mobileFlow = new MobilePageObjects(appiumDriver);
+    @Test
+    public void test001_StepInFB() throws Exception{
+        Assert.assertTrue(mobileFlow.searchOnGoogle(), "Unable to search in Google");
+        Assert.assertTrue(mobileFlow.clickFbPost(), "Unable to click on FB Post");
+        Assert.assertTrue(mobileFlow.openFbPost(), "Unable to Open FB Post");
+        Assert.assertTrue(mobileFlow.openPhotosPost(), "Unable to Open Photos");
     }
-	
-	
+    
+    @Test
+    public void test002_DownloadPhotosAndCheckSize() throws Exception{
+        mobileFlow.clickImage();
+        int imageCounter=1;
+        FileUtils fileUtils = new FileUtils();
+        while(imageCounter<=5) {
+            String filePath = System.getProperty("user.dir") + "/src/test/resources/mobilephotos/" + imageCounter + ".jpg";
+            int size = fileUtils.getFileSizeInKb(filePath);
+            Assert.assertTrue(size > 0, "Image Size is not greater than O KB for Image: " +  imageCounter);
+        }
+    }
+    
 }
+
+
