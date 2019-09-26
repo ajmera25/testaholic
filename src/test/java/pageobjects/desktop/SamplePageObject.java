5ba2d5f75c62f13f27e25dfba3ad3f78a31982fa
Jyoti Chabria <chabriasimran@gmail.com>
2019-09-26 11:09:39 +0530

    Added getAllAlbumNames

--- src/test/java/pageobjects/desktop/SamplePageObject.java
+++ src/test/java/pageobjects/desktop/SamplePageObject.java
@@ -1,4 +1,8 @@
 package pageobjects.desktop;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,6 +11,12 @@ public class SamplePageObject extends BasePage{
 
 	@FindBy(xpath="//span[text()='Photos']")
 	WebElement photos;
+	
+	@FindBy(xpath="//a[2]/div/div")
+	List<WebElement> movieName_list;
+	
+	@FindBy(xpath="//div[contains(text(),'photos') and not(contains(text(),'Cover'))]")
+	List<WebElement> number_list;
 	public SamplePageObject(WebDriver driver) {
 		super(driver);
 	}
@@ -29,5 +39,16 @@ public class SamplePageObject extends BasePage{
 		webDriverClient.click("//span[text()='Photos']");
 
 		webDriverClient.click("//div[text()='Albums']/parent::div/following-sibling::div/a/div[text()='See All']");
+	}
+	
+	public HashMap<String, Integer> getAllAlbumNames() throws Exception {
+		HashMap<String,Integer> albumDetails = new HashMap<>();
+		for(int i=0;i<movieName_list.size();i++) {
+			String albumName=movieName_list.get(i).getText();
+			String number_of_photos=number_list.get(i).getText().split(" ")[0];
+			int noOfPhotos = Integer.valueOf(number_of_photos);
+			albumDetails.put(albumName, noOfPhotos);
+		}
+		return albumDetails;
 	}
 }
