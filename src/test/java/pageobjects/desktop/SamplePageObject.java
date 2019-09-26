26b77d58b07e92eec560e595b5ee6cf4a05c6e90
Amol Chavan <2amolchavan@gmail.com>
2019-09-26 10:28:55 +0530

    POM and test is added

--- src/test/java/pageobjects/desktop/SamplePageObject.java
+++ src/test/java/pageobjects/desktop/SamplePageObject.java
@@ -1,13 +1,29 @@
 package pageobjects.desktop;
-
 import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import core.BasePage;
-
 public class SamplePageObject extends BasePage{
 
+	@FindBy(xpath="//span[text()='Photos']")
+	WebElement photos;
 	public SamplePageObject(WebDriver driver) {
 		super(driver);
 	}
 
+	public void search() throws Exception {
+		webDriverClient.setTextAndEnter("//input[@type='text']", "step-in forum facebook");
+		webDriverClient.click("//*[@id='rso']/div/div/div[6]/div/div/div[1]/a/h3/div");
+	}
+
+	public void navigateToPosts() throws Exception {
+		webDriverClient.click("//span[text()='Posts']");
+	}
+
+	public void navigateToPhotos() throws Exception {
+		webDriverClient.scrollElementToCentre(photos);
+		webDriverClient.click("//span[text()='Photos']");
+
+		webDriverClient.click("//div[text()='Albums']/parent::div/following-sibling::div/a/div[text()='See All']");
+	}
 }
--- src/test/java/tests/SampleWeb.java
+++ src/test/java/tests/SampleWeb.java
@@ -3,12 +3,17 @@ package tests;
 import org.testng.annotations.Test;
 
 import core.BaseTest;
+import pageobjects.desktop.SamplePageObject;
 
 public class SampleWeb extends BaseTest{
-	
-	@Test
-    public void method(){
+
+    @Test
+    public void stepInFB() throws Exception{
+
+        SamplePageObject obj = new SamplePageObject(driver);
+        obj.search();
+        obj.navigateToPosts();
+        obj.navigateToPhotos();
     }
-	
 	
 }
