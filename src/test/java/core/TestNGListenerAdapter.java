8ff84302e2081c2487c2d8063bdb1b5a097ee070
Sachin Kadukar <sachink1586@gmail.com>
2019-09-27 01:53:57 +0530

    Adding extent report

--- src/test/java/core/TestNGListenerAdapter.java
+++ src/test/java/core/TestNGListenerAdapter.java
@@ -2,68 +2,74 @@ package core;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.Augmenter;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.ISuiteListener;
+import org.testng.ITestContext;
 import org.testng.ITestListener;
-import org.testng.ITestNGMethod;
 import org.testng.ITestResult;
-import org.testng.Reporter;
-import org.testng.TestListenerAdapter;
 
-public class TestNGListenerAdapter extends TestListenerAdapter implements ISuiteListener, ITestListener {
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
-	public ThreadLocal<RemoteWebDriver> threadDriver = null;
+public class TestNGListenerAdapter implements ITestListener {
+
+	protected static ExtentReports reports;
+	protected static ExtentTest test;
+
+	public void onTestStart(ITestResult result) {
+		System.out.println("on test start");
+		test = reports.startTest(result.getMethod().getMethodName());
+		test.log(LogStatus.INFO, result.getMethod().getMethodName() + "test is started");
+	}
+
+	public void onTestSuccess(ITestResult result) {
+		System.out.println("On test success");
+		test.log(LogStatus.PASS, result.getMethod().getMethodName() + "test is passed");
+	}
 	
-	@Override
 	public void onTestFailure(ITestResult result) {
-		try {
-			this.threadDriver = new ThreadLocal<RemoteWebDriver>();
-			takeScreenShot(result, threadDriver.get());
+		System.out.println("On test failure");
+		String platform = result.getInstanceName();
+		DriverManagerFactory driverFactory = new DriverManagerFactory();
+		WebDriver driver;
+		if (platform.contains("Desktop")) {
+			driver = driverFactory.getDesktopWebDriver();
+		} else {
+			driver = driverFactory.getAppiumDriver();
 		}
-		catch (Exception e) {
+		
+		test.log(LogStatus.FAIL, result.getMethod().getMethodName() + "test is failed");
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File src = ts.getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(src, new File(System.getProperty("user.dir") + "/src/test/resources/screenshots/" + result.getMethod().getMethodName() + ".png"));
+			String file = test.addScreenCapture(System.getProperty("user.dir") + "/src/test/resources/screenshots/" + result.getMethod().getMethodName() + ".png");
+			test.log(LogStatus.FAIL, result.getMethod().getMethodName() + "test is failed", file);
+			test.log(LogStatus.FAIL, result.getMethod().getMethodName() + "test is failed", result.getThrowable().getMessage());
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		super.onTestFailure(result);
+	}
+	
+	public void onTestSkipped(ITestResult result) {
+		System.out.println("On test skipped");
+		test.log(LogStatus.SKIP, result.getMethod().getMethodName() + "test is skipped");
+	}
+	
+	public void onStart(ITestContext context) {
+		System.out.println("On start");
+		reports = new ExtentReports(System.getProperty("user.dir") + "/target/" + new SimpleDateFormat("yyyy-MM-dd hh-mm").format(new Date()) + "_Report.html");
+	}
+	
+	public void onFinish(ITestContext context) {
+		System.out.println("On finish");
+		reports.endTest(test);
+		reports.flush();
 	}
 
-	public static  void takeScreenShot(ITestResult result, WebDriver driver) {
-		try {
-			driver.switchTo().defaultContent();
-			WebDriver augmentedDriver = new Augmenter().augment(driver);
-			ITestNGMethod testMethod = result.getMethod();
-			File screenshot = ((TakesScreenshot) augmentedDriver).getScreenshotAs(OutputType.FILE);
-			String nameScreenshot = ((RemoteWebDriver) driver).getCapabilities().getBrowserName() + "_" + testMethod.getTestClass().getRealClass().getSimpleName() + "_" + testMethod.getMethodName();
-			String path = getFullPath(nameScreenshot);
-			Reporter.log("Path in take screenshot" +path);
-			FileUtils.copyFile(screenshot, new File(path));
-			Reporter.log ("\n failure screenshot:");
-			Reporter.log ("\n getFileName:" +getFileName(nameScreenshot));
-
-			Reporter.log("<img src='../../screenShots/" + getFileName(nameScreenshot) +"'"+ " target='_blank' >" + "<br>" + getFileName(nameScreenshot) );
-
-		}catch (IOException e) {
-			e.printStackTrace();
-		}
-	}	
-
-	private static String getFileName(String nameTest) throws IOException {
-		Date date = new Date();
-		DateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy_hh-mm-ss");	
-		return dateFormat.format(date) + "_" + nameTest + ".png";
-	}
-
-	private static  String getFullPath(String nameTest) throws IOException {
-		File directory = new File(".");
-		System.out.println("Path in getpath" +directory.getCanonicalPath());
-		String newFileNamePath = directory.getCanonicalPath() + "//screenShots//" + getFileName(nameTest);
-		return newFileNamePath;
-	}
 }
