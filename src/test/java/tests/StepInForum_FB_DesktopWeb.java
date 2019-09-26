b36998c09df0bf433b1a1184a15ab03a1e1a9a0c
Sachin Kadukar <sachink1586@gmail.com>
2019-09-26 12:28:38 +0530

    Test method changes

--- src/test/java/tests/StepInForum_FB_DesktopWeb.java
+++ src/test/java/tests/StepInForum_FB_DesktopWeb.java
@@ -10,23 +10,31 @@ import pageobjects.desktop.FacebookPage;
 import pageobjects.desktop.SamplePageObject;
 
 public class StepInForum_FB_DesktopWeb extends BaseTest{
-
+	
+	SamplePageObject desktop = null;
+	FacebookPage facebook = null;
+	
     @Test
     public void stepInFB() throws Exception{
-        SamplePageObject desktop = new SamplePageObject(driver);
+    	desktop = new SamplePageObject(driver);
+    	facebook = new FacebookPage(driver);
         desktop.search("step-in forum facebook");
         desktop.clickOnResultWithText("25000 test professionals");
-        Assert.assertTrue(desktop.isStepInForumFBPageDisplayed(),"Error! Unable to navigate to Step In forum facebook page");
-        Assert.assertTrue(desktop.navigateToPosts(),"Unable to navigate to posts");
-        Assert.assertTrue(desktop.navigateToAlbums(),"Unable to navigate to all albums");
-        HashMap<String, Integer> finnall = desktop.getAllAlbumNames();
-        System.out.println("Albums: \n"+finnall);
+        Assert.assertTrue(facebook.isStepInForumFBPageDisplayed(),"Error! Unable to navigate to Step In forum facebook page");
+        //Assert.assertTrue(desktop.navigateToPosts(),"Unable to navigate to posts");
     }
     
-    @Test
-	public void test01() {
-		FacebookPage facebook = new FacebookPage(driver);
-		facebook.downloadPhotos();
+    @Test(dependsOnMethods = "stepInFB")
+	public void verifyDownloadPhotos() {
+		Assert.assertTrue(facebook.downloadPhotos(), "Failed to download photos");
+		Assert.assertTrue(facebook.verifyPhotoSize(), "Photo size is not as expected");
 	}
 	
+    @Test(dependsOnMethods = "verifyDownloadPhotos")
+	public void getAlbumNamesAndPhotoCount() throws Exception {
+    	Assert.assertTrue(facebook.navigateToAlbums(),"Unable to navigate to all albums");
+        HashMap<String, Integer> albumNames = facebook.getAllAlbumNames();
+        System.out.println("Albums: \n"+albumNames);
+	}
+    
 }
